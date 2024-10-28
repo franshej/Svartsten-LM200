2c6c71add5579a13a79cf18bbb066e51450106b9
Frans <frans.rosencrantz@gmail.com>
2024-10-28 10:21:07 +0100

    Add blue, yellow, grey amp colours

---
+++ source/AmpColours.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <juce_graphics/juce_graphics.h>  // Include the appropriate JUCE header
+
+#include <cstdint>
+
+struct AmpColours {
+  static const juce::uint32 background{0xff0e0f14};
+  static const juce::uint32 blue{0xffb3effa};
+  static const juce::uint32 yellow{0xfffff586};
+  static const juce::uint32 grey{0xffcacfd3};
+};
